b91e78964f1ea9e921257b7c41d064dd8d926e68
alexohotnikov <alexohotnikov@users.noreply.github.com>
2020-08-29 10:29:51 +0000

    deploy: 615ca6bcb8a77210d1a99f40bdda6726e6e548d2

---
+++ precache-manifest.ccda20c059e8db2480fb35c925a81e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51206160bd9d6f7f483c7c767ed24217",
+    "url": "/react-where-is-iss/index.html"
+  },
+  {
+    "revision": "7f4403ce2cb629ce2c40",
+    "url": "/react-where-is-iss/static/css/2.7151161e.chunk.css"
+  },
+  {
+    "revision": "7f4403ce2cb629ce2c40",
+    "url": "/react-where-is-iss/static/js/2.1e7cd23f.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/react-where-is-iss/static/js/2.1e7cd23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c6055ab062169c2952",
+    "url": "/react-where-is-iss/static/js/main.53f86095.chunk.js"
+  },
+  {
+    "revision": "a9485ab9b161b2cb3c93",
+    "url": "/react-where-is-iss/static/js/runtime-main.209d2a68.js"
+  }
+]);
